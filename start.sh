84acfcea5d9553a87f10584ee04e55752c37fc02
achukuttanTG <104365884+achukuttanTG@users.noreply.github.com>
2023-04-01 12:56:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DKBOTx/private-bot.git /private-bot
+  git clone https://github.com/achukuttanTG/pooodey.git /pooodey
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /private-bot
+  git clone $UPSTREAM_REPO /pooodey
 fi
-cd /private-bot
+cd /pooodey
 pip3 install -U -r requirements.txt
 echo "sᴛᴀʀᴛɪɴɢ 𝗗𝗞 𝗕𝗢𝗧𝘅........"
 python3 bot.py
